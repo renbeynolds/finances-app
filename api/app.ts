708cb76ab6ec467869d966f31bfe1ab44ad2a5f9
Ben Reynolds <breynolds.ctr@kr.af.mil>
2020-10-08 19:09:04 -0400

    Set up typeorm

--- api/app.ts
+++ api/app.ts
@@ -1,11 +1,50 @@
 import express from 'express';
+import { createConnection } from 'typeorm';
+// import { User } from "./v1/entity/User";
 import v1router from './v1/routes';
-// import v1router = require('./v1/routes');
 
 const app = express();
 app.use('/v1', v1router);
 
+createConnection().then(async connection => {
+    // console.log("Inserting a new user into the database...");
+    // const user = new User();
+    // user.firstName = "Timber";
+    // user.lastName = "Saw";
+    // user.age = 25;
+    // await connection.manager.save(user);
+    // console.log("Saved a new user with id: " + user.id);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    // console.log("Loading users from the database...");
+    // const users = await connection.manager.find(User);
+    // console.log("Loaded users: ", users);
+
+    app.listen(3000, function () {
+      console.log('Example app listening on port 3000!');
+    });
 });
+
+
+
+
+
+
+// import "reflect-metadata";
+
+// createConnection().then(async connection => {
+
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
--- api/v1/models/Tag.ts
+++
@@ -1,9 +0,0 @@
-class Tag {
-
-    name: string;
-  
-    constructor(name: string) {
-      this.name = name;
-    }
-
-}
