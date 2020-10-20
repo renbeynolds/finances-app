3b323d63b66172cf7c741d7d287e6696c369bbbc
Ben Reynolds <breynolds.ctr@kr.af.mil>
2020-10-19 22:09:26 -0400

    Can do dynamic where on chart queries now for shared filters

--- api/src/service/ChartService.ts
+++ api/src/service/ChartService.ts
@@ -1,16 +1,15 @@
 import { Request, Response } from 'express';
-import { getManager } from 'typeorm';
+import { getRepository } from 'typeorm';
+import { Transaction } from '../entity/Transaction';
 
 export const getSpendingOverTimeData = async(req: Request, res: Response): Promise<void> => {
-    const manager = getManager();
-    const sql = `
-        SELECT
-            -1 * SUM(amount) AS total,
-            CONCAT(MONTHNAME(STR_TO_DATE(MONTH(date), '%m')), ' ', YEAR(date)) as month
-        FROM transaction
-        WHERE amount < 0
-        GROUP BY MONTH(date), YEAR(date)
-    `;
-    const rawData = await manager.query(sql);
-    res.status(200).send(rawData);
+
+    const result = await getRepository(Transaction).createQueryBuilder("transaction")
+        .select(['-1 * SUM(amount) as total', `CONCAT(MONTHNAME(STR_TO_DATE(MONTH(date), '%m')), ' ', YEAR(date)) as month`])
+        // .where({upload: { id: 9}})
+        .andWhere('amount < 0')
+        .groupBy('MONTH(date), YEAR(date)')
+        .getRawMany();
+
+    res.status(200).send(result);
 };
