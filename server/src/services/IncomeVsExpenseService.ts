f18ab28b327ca8d47ae3c00ac12609495b571130
Ben Reynolds <breynolds.ctr@kr.af.mil>
2022-05-08 11:18:05 -0400

    Fix income vs expense chart

--- server/src/services/IncomeVsExpenseService.ts
+++ server/src/services/IncomeVsExpenseService.ts
@@ -1,20 +1,22 @@
 import { Request, Response } from 'express';
+import { DateTime } from 'luxon';
 import { getManager } from 'typeorm';
 
 export const getIncomeVsExpenseData = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-  // const startDate = req.query.startDate;
-  // const endDate = req.query.endDate;
-  const startDate = '01-01-2020';
-  const endDate = '12-31-2021';
+  const endDate = DateTime.now().endOf('month');
+  const startDate = endDate.minus({ years: 1 });
+
+  const endDateString = endDate.toFormat('LL-dd-yyyy');
+  const startDateString = startDate.toFormat('LL-dd-yyyy');
 
   const manager = getManager();
 
   const rawData = await manager.query(`
     WITH calendar AS (
-      SELECT DATE_TRUNC('month', bucket::date) AS month FROM generate_series('${startDate}', '${endDate}', '1 month'::interval) bucket
+      SELECT DATE_TRUNC('month', bucket::date) AS month FROM generate_series('${startDateString}', '${endDateString}', '1 month'::interval) bucket
     )
     SELECT
       TO_CHAR(c.month, 'YYYY-MM') as month,
@@ -23,8 +25,9 @@ export const getIncomeVsExpenseData = async (
       SUM (CASE WHEN amount < 0 THEN amount ELSE 0 END) AS "Expense"
     FROM calendar c
     LEFT JOIN transaction t ON DATE_TRUNC('month', t.date) = c.month
-    WHERE t."tagId" <> (SELECT id FROM tag WHERE name = 'TRANSFER')
+    WHERE t."tagId" IS NULL OR t."tagId" <> (SELECT id FROM tag WHERE name = 'TRANSFER')
     GROUP BY c.month
+    ORDER BY c.month ASC
   `);
 
   res.status(200).send(rawData);
