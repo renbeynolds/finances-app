8d7c6dcc1f6df6c04c81854c644a334c7cbd34f5
Ben Reynolds <breynolds.ctr@kr.af.mil>
2025-03-11 20:02:51 -0400

    Add trends placeholder

--- frontend/src/components/MainContent.tsx
+++ frontend/src/components/MainContent.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router';
 import Snapshot from './Snapshot';
+import Trends from './Trends';
 import UploadForm from './UploadForm';
 
 export default function MainContent() {
   return (
     <Routes>
       <Route path='/snapshot' element={<Snapshot />} />
+      <Route path='/trends' element={<Trends />} />
       <Route path={':accountId'}>
         <Route path='upload' element={<UploadForm />} />
       </Route>
---
+++ frontend/src/components/Trends.tsx
@@ -0,0 +1,5 @@
+import { Text } from '@mantine/core';
+
+export default function Trends() {
+  return <Text>Trends</Text>;
+}
