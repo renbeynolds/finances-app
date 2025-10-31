e6b0e25e3f009e952e647dd5d4440a36c1156ecf
Ben Reynolds <breynolds.ctr@kr.af.mil>
2025-10-30 21:51:28 -0400

    Fix bank accounts endpoint

--- frontend/src/data/BankAccounts/fetchers.ts
+++ frontend/src/data/BankAccounts/fetchers.ts
@@ -1,22 +1,22 @@
-import { Fetcher } from 'swr';
-import { AmountOverTime } from '../AmountOverTime';
-import { Response } from '../Response';
-import { BankAccount } from './types';
+import { Fetcher } from "swr";
+import { AmountOverTime } from "../AmountOverTime";
+import { Response } from "../Response";
+import { BankAccount } from "./types";
 
-export const BankAccountsEndpoint = '/api/accounts';
+export const BankAccountsEndpoint = "/api/bank_accounts";
 export const BankAccountsFetcher: Fetcher<Response<BankAccount[]>, string> = (
-  url,
+  url
 ) => fetch(url).then((res) => res.json());
 
 export const BankAccountFetcher: Fetcher<Response<BankAccount>, string> = (
-  url,
+  url
 ) => fetch(url).then((res) => res.json());
 
 export const BankAccountBalanceOverTimeEndpoint = (
   accountId: string,
   from: string,
-  to: string,
-) => `/api/accounts/${accountId}/balance_over_time?from=${from}&to=${to}`;
+  to: string
+) => `/api/bank_accounts/${accountId}/balance_over_time?from=${from}&to=${to}`;
 
 export const AmountOverTimeFetcher: Fetcher<
   Response<AmountOverTime[]>,
